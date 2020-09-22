2edf71b16f3b571b95a42e715d85e4f6e81e4176
oriah <oahdout85@gmail.com>
2020-09-22 19:24:24 +0300

    Your Message

---
+++ Emule.Web/Controllers/GeneralController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Emule.Web.Controllers
+{
+  [Route("api")]
+  [ApiController]
+  public class GeneralController : ControllerBase    // api controller
+  {
+    public GeneralController()
+    {
+      
+    }
+
+
+
+
+    [Route("test")]
+    [HttpGet]
+    public IActionResult Test()
+    {
+      return Ok("Hi from Ron's Site");
+    }
+
+    [Route("testcalc")]
+    [HttpGet]
+    public IActionResult TestCalc(int num1, int num2, int num3)
+    {
+      return Ok(num1 + num2 + num3);
+    }
+  }
+}
---
+++ Emule.Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Emule.Web.Models;
+
+namespace Emule.Web.Controllers
+{
+  public class HomeController : Controller    //mvc controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
+
+
+
+
+
+
+    public IActionResult Test()
+    {
+      return Ok("Hi from Ron's Site");
+    }
+
+    public IActionResult TestCalc(int num1, int num2, int num3)
+    {
+      return Ok(num1 + num2 + num3);
+    }
+  }
+}
